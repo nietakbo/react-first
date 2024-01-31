09bd82e7964e7e55047480fa18ee6fbd57088974
Julia Nowak <julianowak077@gmail.com>
2024-01-31 20:35:41 +0100

    add  column/card

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,24 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+
+const ColumnForm = props => {
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title:title , icon:icon });
+        setTitle('');
+        setIcon('')
+    }
+	return (
+        <form onSubmit={handleSubmit} className={styles.columnForm}>
+        <span className={styles.text}>Title:</span> <TextInput value={title} onChange={e => setTitle(e.target.value)} /> 
+        <span className={styles.text}>Icon:</span> <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
+        <Button>ADD COLUMN</Button>
+        </form>
+	);
+};
+
+export default ColumnForm;
